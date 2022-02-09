364e48241e2a319532a22cb386222228d00320b3
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-02-09 14:08:15 +0300

    GetById and Update Methods

---
+++ BookOperationsPatika/Models/DataGenerator.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookOperationsPatika.Models
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Context(serviceProvider.GetRequiredService<DbContextOptions<Context>>()))
+            {
+                if (context.Books.Any())
+                    return;
+                context.Books.AddRange(
+                    new Book{
+                        Id = 1,
+                        Title = "1.book",
+                        GenreType = "science fiction",
+                        PageCount = 234,
+                        PublishDate = "12/03/1999"
+                    },
+                    new Book
+                    {
+                        Id = 2,
+                        Title = "2.book",
+                        GenreType = "science",
+                        PageCount = 455,
+                        PublishDate = "11/1/1111"
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
+
